13b69d83ab9ac2abb0d9282d8c950e3565e8f3a6
FuryRabbit <2294634939@qq.com>
2017-10-28 11:26:12 +0800

    delete useless code；

--- Assets/Clipboard.mm
+++ Assets/Clipboard.mm
@@ -12,10 +12,6 @@
 
 extern "C"
 {
-    /*  compare the namelist with system processes  */
-    void _copyTextToClipboard(const char *textList);
-    const char * _getTextFromClipboard();
-    char* _MakeStringCopy( const char* string);
     float getiOSBatteryLevel();
 }
 
@@ -23,14 +19,7 @@ extern "C"
 
 
 @implementation Clipboard
-//将文本复制到IOS剪贴板
-- (void)objc_copyTextToClipboard : (NSString*)text
-{
-    UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
-    pasteboard.string = text;
-}
 
-//获取IOS剪贴板
 
 @end
 
@@ -38,49 +27,5 @@ extern "C" {
     float getiOSBatteryLevel(){
         [[UIDevice currentDevice] setBatteryMonitoringEnabled:YES];
         return [[UIDevice currentDevice] batteryLevel];
-    }
-    
-    static Clipboard *iosClipboard;
-    void _copyTextToClipboard(const char *textList)
-    {
-        NSString *text = [NSString stringWithUTF8String: textList] ;
-        if(iosClipboard == NULL)
-        {
-            iosClipboard = [[Clipboard alloc] init];
-        }
-        [iosClipboard objc_copyTextToClipboard: text];
-    }
-    
-    char* _MakeStringCopy( const char* string)
-    {
-        if (NULL == string) {
-            return NULL;
-        }
-        char* res = (char*)malloc(strlen(string)+1);
-        strcpy(res, string);
-        return res;
-    }
-    
-    const char * _getTextFromClipboard()
-    {
-        UIPasteboard* pBoard=[UIPasteboard generalPasteboard];
-        if(pBoard!=NULL)
-        {
-            NSString* pNsStr=pBoard.string;
-            if(pNsStr!=NULL)
-            {
-                return _MakeStringCopy([pNsStr UTF8String]);
-            }
-            else
-            {
-                NSLog(@"pBoard.string is null");
-                return _MakeStringCopy("");
-            }
-        }
-        else
-        {
-            NSLog(@"UIPasteboard pBoard is null");
-            return  _MakeStringCopy("");
-        }
     }
 }
